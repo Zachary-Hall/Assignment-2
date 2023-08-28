5dadf2962dd353dd774c8f3d1da477da9b07ce26
Zach <Zachary.hall3@g.austincc.edu>
2023-08-27 21:36:12 -0500

    changed char to *str

--- functions.h
+++ functions.h
@@ -4,9 +4,10 @@
 * 8/24/23
 * ****************************/
 
+
 #ifndef COMMANDLINE_FUNCTIONS_H
 #define COMMANDLINE_FUNCTIONS_H
-
+int string_length(char *str);
 
 
 #endif //COMMANDLINE_FUNCTIONS_H
